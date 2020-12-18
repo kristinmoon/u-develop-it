41015af76cf458bc8e8a4814ba468758e644d28d
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-12-18 00:09:40 -0700

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const inputCheck = require('./utils/inputCheck');
 const sqlite3 = require('sqlite3').verbose();
-
 const express = require('express');
 
 const PORT = process.envPORT || 3001;
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to  database
+// Connect to database
 const db = new sqlite3.Database('./db/election.db', err => {
   if (err) {
     return console.error(err.message);
@@ -17,7 +17,7 @@ const db = new sqlite3.Database('./db/election.db', err => {
   console.log('Connected to the election database.');
 });
 
-// Get all candidates
+// GET all candidates
 app.get('/api/candidates', (req, res) => {
   const sql = `SELECT * FROM candidates`;
   const params = [];
@@ -66,16 +66,30 @@ app.delete('/api/candidate/:id', (req, res) => {
   });
 });
 
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// db.run(sql, params, function (err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
+// CREATE a candidate
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+      id: this.lastID
+    });
+  });
+});
+
+
 
 // Default response for any other requests (not found) catch all
 app.use((req, res) => {
